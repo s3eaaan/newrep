7972bf791f41e40560d2fee0938c12ae8fee70dd
s3eaaan <sean.reyes@student.hhs.school.nz>
2025-10-05 19:39:29 +1300

    External JS and CSS file

---
+++ script.js
@@ -0,0 +1,39 @@
+function openNav() {
+    var sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("open");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var h1 = document.querySelector('#intro > h1');
+    var h2 = document.querySelector('#intro > button');
+    if (!h1) return;
+    var observer = new window.IntersectionObserver(function(entries) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                h1.classList.add('slide-in');
+                h2.classList.add('slide-in');
+            } else {
+                h1.classList.remove('slide-in');
+                h2.classList.remove('slide-in');
+            }
+        });
+    }, { threshold: 0.1 });
+    observer.observe(h1);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var intro = document.getElementById('intro');
+    var logo = document.querySelector('.logo');
+    var observer = new window.IntersectionObserver(function(entries) {
+        entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+            logo.classList.add('logo-hidden');
+            logo.classLis.remove('logo-shown');
+        } else {
+            logo.classList.add('logo-shown');
+            logo.classList.remove('logo-hidden');
+        }
+        });
+    }, { threshold: 0.1 });
+    observer.observe(intro);
+    });
